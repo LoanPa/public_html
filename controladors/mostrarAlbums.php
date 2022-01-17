8083bd65d64543b6e069d130d4c9009254b3bbb8
LoanPa <64327463+LoanPa@users.noreply.github.com>
2022-01-17 19:59:46 +0100

    Eliminat duplicat

--- controladors/mostrarAlbums.php
+++
@@ -1,3 +0,0 @@
-<?php foreach ($albums as $album): ?>
-   <a href="javascript:DetallProducte(<?php echo $album['id'] ?>)"><img src=<?php echo $album['foto'] ?> alt=<?php echo $album['alt_foto'] ?>></a>
-<?php endforeach;?>
