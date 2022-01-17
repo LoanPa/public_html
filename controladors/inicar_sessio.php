1c05d6051779df799244d549904989edfa0b814c
LoanPa <64327463+LoanPa@users.noreply.github.com>
2022-01-17 10:40:17 +0100

    Delete inicar_sessio.php

--- controladors/inicar_sessio.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require_once __DIR__."/../models/connectaBD.php";
-
-    require __DIR__."/../models/consultaHashUsuari.php";
-
-    password_verify(string $password, string $hash): bool
-
-?>
