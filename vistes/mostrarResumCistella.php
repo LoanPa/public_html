df0028bcea27973f3d43fdb58e39eb176c732243
LoanPa <64327463+LoanPa@users.noreply.github.com>
2022-01-18 19:09:08 +0100

    Create mostrarResumCistella.php

---
+++ vistes/mostrarResumCistella.php
@@ -0,0 +1,6 @@
+<?php $preu='preuQueVeDelPHP'; $quantitat='QuantitatQueVeDelPHP'?>
+<div id="shoppingCartDropDown" class="dropDown">
+    <p>Preu <b><?php echo $preu ;?></b></p>
+    <hr>
+    <p>Quantitat <b><?php echo $quantitat;?></b></p>
+</div>
